62fbbb7596a557e39918ab61e22741fe3a7c7565
Lanh123w <161751776+Lanh123w@users.noreply.github.com>
2025-08-11 18:12:36 +0700

    Update ViewComponent.cs

--- Models/ViewComponent.cs
+++ Models/ViewComponent.cs
@@ -1,34 +1,48 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using WEBDOAN.Models;
+using WEBDOAN.Data;
+using WEBDOAN.Models;
 
-namespace WEBDOAN.Models
+namespace WEBDOAN.ViewComponents
 {
     public class UserStatsViewComponent : ViewComponent
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly IOnlineUserService _onlineUserService;
+        private readonly ApplicationDbContext _context;
 
-        public UserStatsViewComponent(UserManager<IdentityUser> userManager, IOnlineUserService onlineUserService)
+        public UserStatsViewComponent(
+            UserManager<IdentityUser> userManager,
+            IOnlineUserService onlineUserService,
+            ApplicationDbContext context)
         {
             _userManager = userManager;
             _onlineUserService = onlineUserService;
+            _context = context;
         }
+
         public class UserStatsViewModel
         {
             public int TotalUsers { get; set; }
             public int OnlineUsers { get; set; }
+            public int TodayVisitCount { get; set; }
+            public int TotalVisitCount { get; set; }
         }
+
         public IViewComponentResult Invoke()
         {
+            var today = DateTime.Today;
+
             var model = new UserStatsViewModel
             {
                 TotalUsers = _userManager.Users.Count(),
-                OnlineUsers = _onlineUserService.GetOnlineCount()
+                OnlineUsers = _onlineUserService.GetOnlineCount(),
+                TodayVisitCount = _context.VisitLogs.Count(v => v.VisitDate.Date == today),
+                TotalVisitCount = _context.VisitLogs.Count()
             };
+
             return View(model);
         }
-
-       
     }
-
 }
